e96842a32a57b4c26b99aaaeab6e93ee43e27a6b
Darpanshah03 <vu2f2425057@pvppcoe.ac.in>
2026-09-19 20:36:16 +0530

    loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div className="loading-screen">
+      <div className="loading-content">
+        <div className="loading-spinner" />
+        <p>Loading NSS Connect...</p>
+      </div>
+    </div>
+  );
+}
